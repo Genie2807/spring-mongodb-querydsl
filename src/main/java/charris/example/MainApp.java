6919f395835b804b60d87c4e44bf1b1b59a36724
Chris Harris <cj_harris5@hotmail.com>
2011-09-26 22:44:36 +0100

    formatting

--- src/main/java/charris/example/MainApp.java
+++ src/main/java/charris/example/MainApp.java
@@ -9,22 +9,22 @@ import charris.example.repositories.ProductRepository;
 
 public class MainApp {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		//get hold of the ProductRepository 
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext("springdata-context.xml");
-		
-		ProductRepository repo = context.getBean(ProductRepository.class);
+        // get hold of the ProductRepository
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("springdata-context.xml");
 
-		//insert a new product
-		repo.save(new Product("SKU-12"));
-	    
-		//query the repo 
-	    Product p = repo.findOne(QProduct.product.sku.eq("SKU-12"));
-	    
+        ProductRepository repo = context.getBean(ProductRepository.class);
+
+        // insert a new product
+        repo.save(new Product("SKU-12"));
+
+        // query the repo
+        Product p = repo.findOne(QProduct.product.sku.eq("SKU-12"));
+
         System.out.println("Found Product :" + p.getSku());
-        
-        //clean down
+
+        // clean down
         repo.deleteAll();
-	  }
+    }
 }
